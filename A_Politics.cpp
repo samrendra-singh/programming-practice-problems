ed826b7224a04db2bc84d481b0a5f0dd6bec5dbb
solo-source <samrendrapersonal@gmail.com>
2025-10-04 23:22:51 +0530

    Solved some more problems, thinking solution for A_Politics.cpp

---
+++ A_Politics.cpp
@@ -0,0 +1,21 @@
+/** 
+ * @file A_Politics.cpp
+ * @brief TLE LEvel 1 - Week 1 Assignment
+ * @author failsafe
+ * @version 1.0
+ */
+
+ #include <iostream>
+ void solve();
+ using namespace std;
+
+ int main() {
+    int t;
+    cin >> t;
+    while(t--) solve;
+    return 0;
+ }
+
+ void solve() {
+    
+ }
